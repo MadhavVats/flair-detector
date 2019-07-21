4b592c191905a3b9affb0982d52dd7fb8b786faa
Madhav <madhav18046@iiitd.ac.in>
2019-07-21 14:13:51 +0530

    added html

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 import praw
-from flask import Flask
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
  
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-   return 'ook'
-
+   if request.method == 'POST':
+      link = request.form['link']
+      return render_template('pred.html',link=link)
+   return render_template('index.html')
 if __name__ == "__main__":
    app.run()
